aade5e2fed1139d9270e9d91348cad588262b7f5
Mike Watkins <mw@mikewatkins.ca>
2018-03-14 09:24:23 -0700

    include baseURL as hugo parameter

--- exampleSite/dev.sh
+++ exampleSite/dev.sh
@@ -4,8 +4,13 @@ PORT=1313
 # matches first global interface, may not be "eth0" on some systems
 IPADDR=`ip a show scope global | grep -m 1 'inet '| awk '{print $2}' | cut -d / -f1`
 
-echo "Serving on: " $IPADDR ":" $PORT
+BASEURL="http://$IPADDR:$PORT"
+
+echo "Serving on: " $IPADDR ":" $PORT " baseURL: " $BASEURL
 # -v verbose log output
 # -D build including drafts 
-# -F build including future dated content; 
-hugo server -v -D -F --bind $IPADDR -p $PORT -b $IPADDR
+# -F build including future dated content 
+# --disableFastRender enable full re-render on changes
+# --renderToDisk because you want to
+hugo server -v -D -F --disableFastRender --forceSyncStatic --renderToDisk \
+	--bind $IPADDR -p $PORT --baseURL $BASEURL 
